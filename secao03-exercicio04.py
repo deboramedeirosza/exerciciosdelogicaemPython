a4094cceee793e49647faaa03e6cb7e400be3253
Débora Medeiros <103949313+deboramedeirosza@users.noreply.github.com>
2022-08-23 12:18:11 -0300

    Create secao03-exercicio04.py

---
+++ secao03-exercicio04.py
@@ -0,0 +1,11 @@
+# Faça um algoritmo que peça dois números e imprima a soma.
+
+#variáveis
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+
+#processamento
+soma = (num1 + num2)
+
+#processamento
+print("A soma é igual a {0}".format(soma))
