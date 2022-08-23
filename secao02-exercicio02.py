8ac74e9c3fc136e2a7497d50256d0ce66fe94801
Débora Medeiros <103949313+deboramedeirosza@users.noreply.github.com>
2022-08-23 12:17:11 -0300

    Create secao02-exercicio02.py

---
+++ secao02-exercicio02.py
@@ -0,0 +1,12 @@
+# Faça um algoritmo para somar dois números e multiplicar o resultado pelo primeiro número.
+
+#variáveis
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+
+#processamento
+soma = num1 + num2
+multiplicacao = soma * num1
+
+#saída
+print("O resultado da multiplicação é {0}". format(multiplicacao))
