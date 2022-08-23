7218c11426b8d99fffc5e5711e6b4b92df4abdae
Débora Medeiros <103949313+deboramedeirosza@users.noreply.github.com>
2022-08-23 12:18:32 -0300

    Create secao03-exercicio05.py

---
+++ secao03-exercicio05.py
@@ -0,0 +1,10 @@
+# Faça um algoritmo que converta metros para centímetros
+
+#variáveis/entrada
+metros = int(input("Informe o tamanho em metros: "))
+
+#processamento
+cent = (metros * 100)
+
+#saída
+print("{0} metros corresponde a {1} centímetros.". format(metros, cent))
